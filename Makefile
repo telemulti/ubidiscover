e40124eddd1d46cd4bff65c79e074d9541e2dc09
Alex Papazoglou <papazoga@gmail.com>
2014-11-04 19:49:21 -0800

    First commit.

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: ubidiscover
+
+ubidiscover: ubidiscover.o
+	gcc ubidiscover.o -o ubidiscover
+
+clean:
+	rm -f ubidiscover.o ubidiscover
---
+++ ubidiscover.c
@@ -0,0 +1,218 @@
+/* 
+ * Parse a Ubiquity discovery packet
+ */
+#include <arpa/inet.h>
+#include <malloc.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+void barf(char *s)
+{
+    printf("error: %s\n", s);
+    exit(EXIT_FAILURE);
+}
+
+
+void spaces(int i)
+{
+  while (i--)
+    fputc(' ', stdout);
+}
+
+void dump_chunk(unsigned char *chunk, int len)
+{
+  int i;
+  for (i=0;i<len;i++) {
+    if (i!=0)
+      fputc(' ', stdout);
+    printf ("%02X", chunk[i]);
+  }
+}
+
+void print_hwaddr(unsigned char *b)
+{
+  int i;
+  for (i=0;i<6;i++) {
+    if (i!=0)
+      fputc(':', stdout);
+    printf ("%02x", b[i]);
+  }
+}
+
+void print_ipv4(unsigned char *b)
+{
+  char str[20];
+  inet_ntop(AF_INET, b, str, 20);
+  fputs(str, stdout);
+}
+
+unsigned int get_number(unsigned char *b, int len)
+{
+  unsigned int n;
+  int i;
+
+  n = 0;
+  for (i=0;i<len;i++) {
+    n <<= 8;
+    n |= b[i];
+  }
+
+  return n;
+}
+
+int decode_chunk(int depth, unsigned char *chunk, unsigned int total_len)
+{
+  unsigned int len;
+  unsigned char type;
+  char *str;
+  int pos = 0;
+  
+  while (pos < total_len) {
+    type = chunk[pos];
+    len = get_number(chunk + pos + 1, 2);
+    pos += 3;
+
+    spaces(depth);
+    if (type == 0) {
+      printf ("discover-message (len %d)\n", len);
+      decode_chunk(depth+2, chunk+pos, len);
+    } else if (type == 0x03) {
+      str = strndup(chunk+pos, len);
+      printf ("firmware '%s'\n", str);
+      free(str);
+    } else if (type == 0x0b) {
+      str = strndup(chunk+pos, len);
+      printf ("name '%s'\n", str);
+      free(str);
+    } else if (type == 0x0c) {
+      str = strndup(chunk+pos, len);
+      printf ("board.shortname '%s'\n", str);
+      free(str);
+    } else if (type == 0x10) {
+      unsigned int sysid;
+      sysid = get_number(chunk+pos, len);
+      printf ("board.sysid 0x%x\n", sysid);
+    } else if (type == 0x0d) {
+      str = strndup(chunk+pos, len);
+      printf ("wireless.ssid '%s'\n", str);
+      free(str);
+    } else if (type == 0x0e) {
+      printf ("wmode 0x%02x\n", chunk[pos]);
+    } else if (type == 0x02) {
+      printf ("iface hwaddr ", str);
+      print_hwaddr(chunk+pos);
+      printf (" ipv4 ");
+      print_ipv4(chunk+pos+6);
+      fputc('\n', stdout);
+    } else if (type == 0x01) {
+      printf ("board.hwaddr ");
+      print_hwaddr(chunk+pos);
+      fputc('\n', stdout);
+    } else if (type == 0x0a) {
+      unsigned int uptime;
+      uptime = get_number(chunk+pos, 4);
+      printf ("uptime %d\n",uptime);      
+    } else {
+      printf("chunk (type 0x%x): ", type);
+      dump_chunk(chunk+pos, len);
+    }
+    pos += len;
+  }
+}
+
+int decode_msg(unsigned char *msg, int total_len)
+{
+  unsigned char magic;
+
+  magic = msg[0];
+  msg++;
+
+  if (magic != 1) {
+    printf ("oops: magic is not 1!\n");
+  }
+  decode_chunk(0, msg, total_len-1);
+}
+
+struct {
+  int sockfd;
+  struct sockaddr_in saddr;
+} ctx;
+
+/*                        version/magic  type    length            ? */
+unsigned char query[] = { 0x01,          0x00,   0x00, 0x00 };
+
+
+int lookup(char *name)
+{
+  struct addrinfo hints;
+  char addrstr[128];
+  struct sockaddr_in *sa;
+  struct addrinfo *ai;
+  int r;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_protocol = IPPROTO_UDP;
+  hints.ai_canonname = NULL;
+  hints.ai_addr = NULL;
+  hints.ai_next = NULL;
+
+  r = getaddrinfo(name, "10001", &hints, &ai);
+
+  if (r<0)
+    barf(strerror(errno));
+
+  /* grab the first address */
+  sa = (struct sockaddr_in *)ai->ai_addr;
+  inet_ntop(AF_INET, &sa->sin_addr, addrstr, ai->ai_addrlen);
+  bcopy(ai->ai_addr, &ctx.saddr, ai->ai_addrlen);
+
+  freeaddrinfo(ai);
+
+  return r;
+}
+
+unsigned char inmsg[4096];
+int inlen;
+
+main(int argc, char **argv)
+{
+  struct sockaddr_in sa;
+  int r;
+  socklen_t sl;
+
+  if (argc < 2)
+    barf("need a hostname");
+
+  if (argc >= 3) {
+    unsigned char t;
+    sscanf(argv[2], "%x", &t);
+    printf ("doing type 0x%x\n", t);
+    query[1]=t;
+  }
+
+  ctx.sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (ctx.sockfd < 0)
+    barf(strerror(errno));
+
+
+  if (lookup(argv[1]) < 0)
+    barf("bad lookup");
+
+  /* trasmit query */
+  r = sendto(ctx.sockfd, query, 4, 0, (struct sockaddr *)&ctx.saddr, sizeof(ctx.saddr));
+  if (r<0)
+    barf(strerror(errno));
+
+  /* collect respose */
+  inlen = recvfrom(ctx.sockfd, inmsg, 4096, 0, (struct sockaddr *)&sa, &sl);
+  decode_msg(inmsg, inlen);
+}
